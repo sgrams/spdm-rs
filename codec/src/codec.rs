e7a4604ed7d24d4d825d6f6c479c2c4946849a5d
Stanislaw Grams <stanislaw.grams@intel.com>
2026-02-06 14:33:54 +0100

    codec: apply cargo check fixes
    Signed-off-by: Stanislaw Grams <stanislaw.grams@intel.com>

--- codec/src/codec.rs
+++ codec/src/codec.rs
@@ -17,7 +17,7 @@ pub struct Reader<'a> {
 }
 
 impl Reader<'_> {
-    pub fn init(bytes: &[u8]) -> Reader {
+    pub fn init(bytes: &[u8]) -> Reader<'_> {
         Reader {
             buf: bytes,
             offs: 0,
@@ -52,7 +52,7 @@ impl Reader<'_> {
         self.offs
     }
 
-    pub fn sub(&mut self, len: usize) -> Option<Reader> {
+    pub fn sub(&mut self, len: usize) -> Option<Reader<'_>> {
         self.take(len).map(Reader::init)
     }
 }
@@ -70,7 +70,7 @@ pub struct Writer<'a> {
 }
 
 impl Writer<'_> {
-    pub fn init(bytes: &mut [u8]) -> Writer {
+    pub fn init(bytes: &mut [u8]) -> Writer<'_> {
         Writer {
             buf: bytes,
             offs: 0,
